f5d83daa9a1908790f096c3496067409cae98f0b
bhanu1414 <bhanukandregula2003@gmail.com>
2024-10-21 16:32:01 +0530

    new photo added

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["UI/UX DEVELOPER / DESIGNER"],
+    typeSpeed: 100, // Increased from 100 to 200 for slower typing
+    backSpeed: 100,
+    backDelay: 1300,
+    loop: true
+});
